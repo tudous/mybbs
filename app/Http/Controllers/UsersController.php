0fb357a79e4edef90d431867f9dbcc47a8348fd9
vagrant <vagrant@homestead>
2018-06-22 14:38:59 +0000

    auth renzhen

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,22 +6,38 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use App\Handlers\ImageUploadHandler;
+use auth;
+use App\Policies\UserPolicy;
 
 
 class UsersController extends Controller
 {
+    public function __construct(){
+        //权限控制,除show方法，其他所有的动作都需要登录才能访问，否则重定向
+        $this->middleware('auth',['except'=>['show']]);
+
+    }
+
+
     public function show(User $user)
     {
         return view('user.show',compact('user'));
     }
     public function edit(User $user)
     {
+        try {
+            $this->authorize('update',$user);
+            return view('user.edit',compact('user'));
+        } catch (\Exception $e) {
 
-        return view('user.edit',compact('user'));
+            abort(500);
+        }
+
+
     }
     public function update(UserRequest $request,ImageUploadHandler $imageUploadHandler,User $user)
     {
-
+        $this->authorize('update',$user);
         //获取数据
         $data=$request->all();
 
