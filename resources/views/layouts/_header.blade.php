0892d526180e7a32de3f6af98deb72082562d84d
vagrant <vagrant@homestead>
2018-06-27 13:21:25 +0000

    user show topics list

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -49,6 +49,9 @@
 
                         <ul class="dropdown-menu" role="menu">
                             <li>
+                            <a href="{{ route('users.show',Auth::id()) }}">个人中心</a>
+                            </li>
+                            <li>
                             <a href="{{ route('users.edit',Auth::id()) }}">编辑资料</a>
                             </li>
                             <li>
