9732ce3e9052c38982056195a79109ab5090141a
vagrant <vagrant@homestead>
2018-07-05 14:49:55 +0000

    admin can manage site setting

--- config/administrator.php
+++ config/administrator.php
@@ -47,7 +47,10 @@ return array(
             'categories',
             'topics',
             'replies',
-        ]
+        ],
+        '站点管理'=>[
+            'settings.site',
+        ],
 
     ],
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-     <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
+     <meta name="description" content="@yield('description', setting('seo_description', 'LaraBBS 爱好者社区。'))" />
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,社区,论坛,开发者论坛'))" />
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS')</title>
+    <title>@yield('title', 'LaraBBS')--- {{ setting('site_name', 'Laravel 进阶教程') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
