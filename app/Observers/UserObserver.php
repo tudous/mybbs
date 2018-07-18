e72413621a525893a9c55d3baafaa3ddb8e71ad8
vagrant <vagrant@homestead>
2018-07-18 14:35:03 +0000

    add dingo

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -23,4 +23,12 @@ class UserObserver
     {
          \DB::table('topics')->where('user_id',$user->id)->delete();
     }
+
+    public function saving(User $user)
+    {
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
-//Auth::routes();
+//Auth::routes();替代下面的登陆注册密码路由
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
